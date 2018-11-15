c957dae2d15e55bb1139d0cee9a9399a78180c7c
valentineezeh <val.ezeh15@gmail.com>
2018-11-15 14:45:32 +0100

    set up webpack react and redux - set up webpack - set up and configure react - set up and configure redux

---
+++ client/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div>
+                    <h1>Hello world welcome</h1>
+                </div>
+            </div>
+        );
+    }
+}
+export default App
