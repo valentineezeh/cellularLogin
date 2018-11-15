26580a5653edc3c0ce9fdd553f77f9656a2f7089
valentineezeh <val.ezeh15@gmail.com>
2018-11-15 10:29:46 +0100

    -set-up-app and database - Set-up express - Configure base application - set-up and configure postgres database - Create User model and migrations

--- server/app.js
+++ server/app.js
@@ -1,29 +1,23 @@
 // bringing in dependencies
-import express from 'express';
-import logger from 'morgan';
-import bodyParser from 'body-parser';
-import path from 'path';
-import cors from 'cors'
-
+import express from "express";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import path from "path";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.static('./client'));
-app.use(logger('dev'));
+app.use(express.static("./client"));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
-// connect all routes to application
-
-
-app.use(cors())
+app.use(cors());
 
 // app.use('/', router);
 
 const port = +process.env.PORT || 8009;
-app.set('port', port);
+app.set("port", port);
 
 // Turn on the server
 
